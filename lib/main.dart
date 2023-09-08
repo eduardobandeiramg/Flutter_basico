b31c12cb69c7a5327b6c79a816ba7a09473ac294
eduardobandeiramg <eduardobandeiramg@gmail.com>
2023-09-08 18:35:25 -0300

    atividade - fazendo varios containers coloridos organizados na tela

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Row (mainAxisAlignment: MainAxisAlignment.center, children: [
-          //Podem ser criadas estruturas que conterão algo (Containers) ou estruturas que terão um conjunto de estruturas, como containers.
-          //(Row, Column, Stack...)
-          Container(color: Colors.white , width: 15, height: 15,),
-          Container(color: Colors.green , width: 20, height: 20,),
-          Container(color: Colors.red , width: 50, height: 50,),
-          Container(color: Colors.blue , width: 100, height: 100,),
-        ],
+        home: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Container(width: 100, height: 100, color: Colors.red,),
+              Container(width: 100, height: 100, color: Colors.orange,),
+              Container(width: 100, height: 100, color: Colors.yellow,),
+            ],
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Container(width: 100, height: 100, color: Colors.green,),
+              Container(width: 100, height: 100, color: Colors.blue[200],),
+              Container(width: 100, height: 100, color: Colors.blueAccent,),
+
+            ],),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              Container(width: 100, height: 100, color: Colors.purple,),
+              Container(width: 100, height: 100, color: Colors.red[800],),
+              Container(width: 100, height: 100, color: Colors.white,),
+            ],),
+          ],
         )
-    );
+
+        );
   }
 
 }
+///Atalhos jmportantes:
+///Opções para envolver elemento: clicar no elemento > option + enter
+///
+///
+///
