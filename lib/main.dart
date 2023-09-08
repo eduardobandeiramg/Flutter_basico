3cccab74ff3ad64359facd6858d5d29f32fa8870
eduardobandeiramg <eduardobandeiramg@gmail.com>
2023-09-08 20:13:34 -0300

    adicionando mais comentarios sobre widgets e seus vários filhos ou filho único

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Column(
+        home: Column(//Column, Row e Stack suportam atributo children. Container suporta apenas child
           //Tambem poderia adicionar a coluna dentro de um container e então os filhos desse container (rows)
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
               Container(width: 100, height: 100, color: Colors.red,),
               Container(width: 100, height: 100, color: Colors.orange,),
               Container(width: 100, height: 100, color: Colors.yellow,),
             ],
             ),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
               Container(width: 100, height: 100, color: Colors.green,),
               Container(width: 100, height: 100, color: Colors.blue[200],),
               Container(width: 100, height: 100, color: Colors.blueAccent,),
 
             ],),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
               Container(width: 100, height: 100, color: Colors.purple,),
               Container(width: 100, height: 100, color: Colors.red[800],),
               Container(width: 100, height: 100, color: Colors.white,),
