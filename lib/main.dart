44625318d906a96324e063eeabf47a4417f16455
eduardobandeiramg <eduardobandeiramg@gmail.com>
2023-09-09 17:13:22 -0300

    finalizando com listview, padding, stateful, LinearProgressionIndicator

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,36 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          floatingActionButton: FloatingActionButton.large(onPressed: (){print("apertou o botao");}),
+          floatingActionButton: FloatingActionButton.large(onPressed: (){print("apertou o botao");},
+          child: Icon(Icons.ios_share),),
           appBar: AppBar(
             title: Text("título"),
           ),
-          body: Container(color: Colors.red,),
+          body: Container(child: ListView(
+            children: [
+              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),
+              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),
+              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),              Container(height: 50, color: Colors.green,),
+              Container(height: 50, color: Colors.yellow,),
+
+            ],
+          ),),
         )
 
         );
@@ -24,7 +49,17 @@ class MyApp extends StatelessWidget {
 
 }
 ///Atalhos jmportantes:
-///Opções para envolver elemento: clicar no elemento > option + enter
+///Opções para envolver elemento: clicar no elemento > option + enter (ou mudar estado da classe)
 ///
 ///
+///Resumo:
 ///
+///widgets(objetos) possuem em seus construtores parametros obrigatorios e opcionais
+///Esses parametros podem definir tanto características do widget como outros widgets que farão parte dele
+///(child ou children).
+///
+///container, row, columns, stack (containers evitam overflow)
+///scaffold
+///listview
+///padding (EdgeInsets. ...)
+///LinearProgressIndicator
